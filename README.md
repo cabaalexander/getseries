0812b3c6f6810e3c7807f6ef6ee7634831a62a87
cabaalexander <cabaalexander321@gmail.com>
2019-02-18 20:37:40 -0400

    feat: 🎸 Make initial fetch

---
+++ README.md
@@ -0,0 +1,7 @@
+# getseries
+
+Package to fetch series ¯\\_(ツ)_/¯
+
+## Hey!
+
+Commits on this PR are made with [commitizen](https://www.npmjs.com/package/commitizen) ;)
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fetch = require('node-fetch')
+const cli = require('cli')
+const chalk = require('chalk')
+
+const downloadColor = chalk.blueBright
+const finishColor = chalk.green
+
+const { GET_SERIES_ENDPOINT } = process.env
+
+cli.spinner(downloadColor('Fetching...'))
+
+fetch(GET_SERIES_ENDPOINT)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .then(_ => { cli.spinner(finishColor('Fetching... done!'), true) })
