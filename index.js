d5769acc71bbb05f447073c5c5272967c3bd161e
cabaalexander <cabaalexander321@gmail.com>
2019-02-21 23:30:28 -0400

    feat: 🎸 Add `eslint` and fix

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch')
-const cli = require('cli')
-const chalk = require('chalk')
+const fetch = require('node-fetch');
+const cli = require('cli');
+const chalk = require('chalk');
 
-const downloadColor = chalk.blueBright
-const finishColor = chalk.green
+const downloadColor = chalk.blueBright;
+const finishColor = chalk.green;
 
-const { GET_SERIES_ENDPOINT } = process.env
+const {GET_SERIES_ENDPOINT} = process.env;
 
-cli.spinner(downloadColor('Fetching...'))
+cli.spinner(downloadColor('Fetching...'));
 
 fetch(GET_SERIES_ENDPOINT)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .then(_ => { cli.spinner(finishColor('Fetching... done!'), true) })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .then((_) => {
+      cli.spinner(finishColor('Fetching... done!'), true);
+    });
