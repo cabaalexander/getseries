021c314e74416ed7269eb97c9591161e343c01d0
cabaalexander <cabaalexander321@gmail.com>
2019-02-28 02:13:26 -0400

    feat: 🎸 Fetch serie all seasons caps

--- index.js
+++ index.js
@@ -6,17 +6,47 @@ const chalk = require('chalk');
 
 const downloadColor = chalk.blueBright;
 const finishColor = chalk.green;
+const log = console.log;
 
-const {GET_SERIES_ENDPOINT} = process.env;
+const {
+  GET_SERIES_ENDPOINT,
+  GET_SERIES_SEASONS_ENDPOINT,
+} = process.env;
+const [,, ...args] = process.argv;
+const serieName = args.join(' ');
+
+if (!serieName) {
+  log('Provide a name pls...');
+  process.exit(1);
+}
+
+const toJson = (res) => res.json();
+
+const handleError = ({message}) => log(message);
+
+
+const seriesByTitle = (title) => ({title: serieTitle}) =>
+  RegExp(title, 'i').test(serieTitle);
+
+
+const filterJson = (series) =>
+  series.filter(seriesByTitle(serieName))[0];
+
+
+const fetchAllSeasons = ({_id}) =>
+  fetch(`${GET_SERIES_SEASONS_ENDPOINT}/${_id}`);
+
 
 cli.spinner(downloadColor('Fetching...'));
 
-const handleError = ({message}) => console.log(message);
 
 fetch(GET_SERIES_ENDPOINT)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
+    .then(toJson)
+    .then(filterJson)
+    .then(fetchAllSeasons)
+    .then(toJson)
+    .then(log)
     .catch(handleError)
-    .finally((_) => {
+    .finally(() => {
       cli.spinner(finishColor('Fetching... done!'), true);
     });
