8814ea0428a9fcafd5447ac9a4aa7b681e5c2ed7
cabaalexander <cabaalexander321@gmail.com>
2019-03-02 00:25:38 -0400

    feat: 🎸 Filter seasons an episodes

--- index.js
+++ index.js
@@ -13,37 +13,58 @@ const {
   GET_SERIES_SEASONS_ENDPOINT,
 } = process.env;
 const [,, ...args] = process.argv;
-const serieName = args.join(' ');
+const SERIE_NAME = args.join(' ');
+const SERIE_SEASON = 3;
+const SERIE_EPISODE = 0;
 
-if (!serieName) {
+
+if (!SERIE_NAME) {
   log('Provide a name pls...');
   process.exit(1);
 }
 
+
 const toJson = (res) => res.json();
 
 
-const handleError = ({message}) => log(message);
+const handleError = ({message}) => log(`[error]\n${message}`);
 
 
 const seriesByTitle = (title) => ({title: serieTitle}) =>
   RegExp(title, 'i').test(serieTitle);
 
 
-const filterJson = (series) =>
-  series.filter(seriesByTitle(serieName))[0];
+const filterSeries = (series) =>
+  series.filter(seriesByTitle(SERIE_NAME))[0];
 
 
-const fetchAllSeasons = ({_id}) =>
+const fetchAllSeasonsEpisodes = ({_id}) =>
   fetch(`${GET_SERIES_SEASONS_ENDPOINT}/${_id}`)
-      .then(toJson);
+      .then(toJson)
+      .then(({episodes}) => episodes);
+
+
+const filterSeasons = (allSeasonEpisodes) =>
+  allSeasonEpisodes.filter(({season}) => season == SERIE_SEASON);
+
+
+const filterEpisodes = (episodes) => {
+  if (!SERIE_EPISODE) {
+    return episodes;
+  }
+  return episodes
+      .filter(({episode}) => episode == SERIE_EPISODE)
+      .map(({content}) => content);
+};
 
 
 cli.spinner(downloadColor('Fetching...'));
 fetch(GET_SERIES_ENDPOINT)
     .then(toJson)
-    .then(filterJson)
-    .then(fetchAllSeasons)
+    .then(filterSeries)
+    .then(fetchAllSeasonsEpisodes)
+    .then(filterSeasons)
+    .then(filterEpisodes)
     .then(log)
     .catch(handleError)
     .finally(() => {
